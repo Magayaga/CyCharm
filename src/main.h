c105de7fdca20d569f294f1673af66387d682982
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2025-03-16 22:46:26 +0800

    Update `main.h` (v1.0-preview3)

--- src/main.h
+++ src/main.h
@@ -1,8 +1,10 @@
 // CyCharm : include file for standard system include files, or project specific include files
-// Copyright 2023-2024 Cyril John Magayaga
+// Copyright 2023-2025 Cyril John Magayaga
 
 #define MAX_TEXT_LENGTH 10000
 #define MAX_HISTORY_SIZE 50
 
 #define IDC_STATUSBAR 1001
 #define IDC_EDIT 1002
+
+#define AUTOSAVE_TIMER_ID 100
