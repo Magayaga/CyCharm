cbd11279d3dd5dd07d12fea6b33441c862abe0e9
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2025-05-15 22:04:26 +0800

    Update `main.h` (v1.0-preview5)

--- src/main.h
+++ src/main.h
@@ -7,4 +7,29 @@
 #define IDC_STATUSBAR 1001
 #define IDC_EDIT 1002
 
+// Status bar parts
+#define SB_PART_POSITION 0
+#define SB_PART_CHARCOUNT 1
+#define SB_PART_ENCODING 2
+#define SB_PART_ZOOM 3
+
 #define AUTOSAVE_TIMER_ID 100
+
+// Global variables for theming
+#define THEME_LIGHT 0
+#define THEME_DARK 1
+#define THEME_SYSTEM 2
+
+// Encoding types
+#define ENCODING_UTF8 0
+#define ENCODING_UTF16LE 1
+#define ENCODING_UTF16BE 2
+#define ENCODING_ASCII 3
+#define ENCODING_ISO_8859_1 4  // Latin-1
+#define ENCODING_ISO_8859_15 5 // Latin-9 (with Euro sign)
+#define ENCODING_WINDOWS_1252 6 // Windows Latin-1
+#define ENCODING_SHIFT_JIS 7    // Japanese
+#define ENCODING_GB18030 8      // Chinese
+
+#define MAX_LINES 1000
+#define MAX_LENGTH 1024
